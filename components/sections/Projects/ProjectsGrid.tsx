62332caf230722f7cfe3834b638bbb4139b07895
Miguel Gresa <josemiguelcpcs@gmail.com>
2026-04-28 23:47:04 +0200

    feat(ProjetsGrid): Added link to project in grid

--- components/sections/Projects/ProjectsGrid.tsx
+++ components/sections/Projects/ProjectsGrid.tsx
@@ -5,6 +5,7 @@ import { motion } from "motion/react";
 import ProjectsTrailer from "./ProjectsTrailer";
 import ProjectsSubtitle from "./ProjectsSubtitle";
 import ProjectSkeleton from "./ProjectSkeleton";
+import Link from "next/link";
 
 export interface ProjectCredit {
   role: string;
@@ -39,19 +40,21 @@ export default function ProjectsGrid({ projects }: ProjectsGridProps) {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16 lg:gap-y-24 lg:gap-x-12">
       {projects.map((project, index) => (
         <Suspense key={project.id} fallback={<ProjectSkeleton />}>
-          <motion.div
-            className="flex flex-col group cursor-pointer"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.5,
-              ease: [0.25, 0.1, 0.25, 1.0],
-              delay: index * 0.15,
-            }}
-          >
-            <ProjectsTrailer videoUrl={project.videoUrl} />
-            <ProjectsSubtitle title={project.title} tag={`${project.type} / ${project.tag}`} />
-          </motion.div>
+          <Link href={`/project`}>
+            <motion.div
+              className="flex flex-col group cursor-pointer"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.5,
+                ease: [0.25, 0.1, 0.25, 1.0],
+                delay: index * 0.15,
+              }}
+            >
+              <ProjectsTrailer videoUrl={project.videoUrl} />
+              <ProjectsSubtitle title={project.title} tag={`${project.type} / ${project.tag}`} />
+            </motion.div>
+          </Link>
         </Suspense>
       ))}
     </div>

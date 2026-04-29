c455e38d4aa5e99fbd463077c75ecf0467391eec
Miguel Gresa <josemiguelcpcs@gmail.com>
2026-04-29 14:46:36 +0200

    fix: Stills -> Selected Media

--- components/sections/Project/ProjectStills.tsx
+++ components/sections/Project/ProjectStills.tsx
@@ -20,7 +20,7 @@ export default function ProjectStills({ stills }: ProjectStillsProps) {
         viewport={{ once: true }}
         transition={{ duration: 0.5, ease: [0.25, 0.1, 0.25, 1.0] }}
       >
-        Film Stills
+        Selected Media
       </motion.h3>
       <div className={containerStyles}>
         {stills.map((still, index) => (
